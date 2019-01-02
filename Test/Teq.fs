dc572d31b8b178f6c00e39602fc64ffe48e21814
Nicholas Cowle <nickcowle@users.noreply.github.com>
2019-01-02 09:03:31 +0000

    Add Teq.tryRefl (#9)

--- Test/Teq.fs
+++ Test/Teq.fs
@@ -7,6 +7,7 @@ open FsUnitTyped
 
 [<TestFixture>]
 module TestTeq =
+
     [<Test>]
     let ``Casting with Teq.refl is id`` () =
         let value = 42
@@ -20,3 +21,13 @@ module TestTeq =
     let ``Cong.list does not throw`` () =
         let t : Teq<int,int> = Teq.refl
         Assert.DoesNotThrow(fun () -> Teq.Cong.list t |> ignore)
+
+    [<Test>]
+    let ``Teq.tryRefl returns a Teq when the types match`` () =
+        let t = Teq.tryRefl<int, int>
+        t |> Option.isSome |> shouldEqual true
+
+    [<Test>]
+    let ``Teq.tryRefl returns None when the types differ`` () =
+        let t = Teq.tryRefl<int, string>
+        t |> Option.isSome |> shouldEqual false
